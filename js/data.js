0f095734d6e78648b7b074aa160013198f7f5f58
Leidy <lmholguin05@gmail.com>
2022-07-12 23:29:26 -0500

    ecommer

---
+++ js/data.js
@@ -0,0 +1,20 @@
+export { productos };
+let productos = [
+    {id:1,
+     name: 'Camisa',
+     image: 'https://w7.pngwing.com/pngs/549/252/png-transparent-black-t-shirt-clothing-t-shirt-clothes-thumbnail.png',
+     price: 200},
+     {id:2,
+     name: 'Pantalon',
+     image: 'https://png.pngtree.com/png-vector/20201127/ourlarge/pngtree-ladies-jeans-png-image_2400806.jpg',
+     price: 150},
+     {id:3,
+     name: 'Tenis',
+     image: 'https://w7.pngwing.com/pngs/30/966/png-transparent-nike-air-max-sneakers-nike-free-shoe-tenis-shoes-white-outdoor-shoe-running-thumbnail.png',
+     price: 60},
+     {id:4,
+     name: 'Medias',
+     image: 'https://w7.pngwing.com/pngs/287/908/png-transparent-tights-knee-black-m-gasp-better-bodies-store-black-shoe-tights.png',
+     price: 10},
+
+]
---
+++ main.js
@@ -0,0 +1,13 @@
+const Array = [
+];
+
+document.addEventListener("DOMContentLoaded", function () {
+    const foods = document.querySelector(".foods");
+
+    let html = "";
+
+    for (let i = 0; i < foods.length; i++) {
+        html += ´ 
+        ´
+    }
+});
